09228b2251ea2053097d48be632076ced7ee227d
Maximilian Weiherer <96751597+mweiherer@users.noreply.github.com>
2025-09-23 11:26:09 +0200

    Fixed but in setup_vggsfm.py

--- scripts/setup_vggsfm.sh
+++ scripts/setup_vggsfm.sh
@@ -1,11 +1,15 @@
 # Small script to download and install VGGSfM.
 
 
-mkdir extern && cd extern
+cd extern
+
 git clone https://github.com/facebookresearch/vggsfm.git
 cd vggsfm
 
 source install.sh
 python -m pip install -e .
+
+conda deactivate 
+conda activate local-irbsm
 
 cd ../..
