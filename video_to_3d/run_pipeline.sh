2ad40ba241c49cbf98311ec78fa5e11db9f9799e
Maximilian Weiherer <96751597+mweiherer@users.noreply.github.com>
2025-09-23 12:09:25 +0200

    Fixed bug in run_pipeline.sh

--- video_to_3d/run_pipeline.sh
+++ video_to_3d/run_pipeline.sh
@@ -6,7 +6,7 @@ base_output_dir="./${filename%.*}_reconstruction/"
 
 python video_to_3d/extract_frames.py $1 $base_output_dir
 python video_to_3d/run_sfm.py $base_output_dir
-python video_to_3d/pick_2d_landmarks.py $base_output_dir
+python video_to_3d/pick_2d_landmarks.py $base_output_dir/images
 
 landmark_file=$(find $base_output_dir -maxdepth 1 -type f -name "*.txt" | head -n 1)
 if [ -z "$landmark_file" ]; then
@@ -15,4 +15,4 @@ if [ -z "$landmark_file" ]; then
 fi
 
 python video_to_3d/align_and_prune.py $base_output_dir $landmark_file
-python ../reconstruct.py $base_output_dir/aligned_pt_pruned.ply --landmarks $base_output_dir/aligned_landmarks_3d.ply --output_dir $base_output_dir --metrical --latent_weight 0.1 --anchor_weight 0.1
+python ./reconstruct.py $base_output_dir/aligned_pt_pruned.ply --landmarks $base_output_dir/aligned_landmarks_3d.ply --output_dir $base_output_dir --metrical --latent_weight 0.1 --anchor_weight 0.1
